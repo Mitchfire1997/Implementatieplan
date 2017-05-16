995409e4e0c47909e810a9991d8c033ac3a83246
Mitchfire1997 <m.woude6@kpnmail.nl>
2017-05-16 13:10:53 +0200

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'u912813865_vyrej');
+define('DB_NAME', '');
 
 /** MySQL database username */
-define('DB_USER', 'u912813865_dudyt');
+define('DB_USER', '');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'implementatie');
+define('DB_PASSWORD', 'geheim');
 
 /** MySQL hostname */
 define('DB_HOST', 'mysql.hostinger.nl');
