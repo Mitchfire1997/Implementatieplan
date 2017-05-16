1d38ef99d741fe17133b90673deb833ed5bfc578
Mitchfire1997 <m.woude6@kpnmail.nl>
2017-05-16 13:22:01 +0200

    Update ticket-plugin.php

--- wp-content/plugins/Ticket-plugin/ticket-plugin.php
+++ wp-content/plugins/Ticket-plugin/ticket-plugin.php
@@ -87,7 +87,7 @@
         </script>
         </head>
         <body>       
-            <form action='http://implementatiekt3.hole.es/ticket' method='post'>
+            <form action='http://.hole.es/ticket' method='post'>
                 <table>
                     <tbody class="bodyClass" >
                 <tr>    
